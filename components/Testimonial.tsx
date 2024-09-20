6d60e3395d5e06e71849da6b641734bcc6bbbb5c
Makibu <bumaciej@gmail.com>
2024-09-20 17:23:48 +0200

    Padding fix
    Fixed hero padding for smaller screens

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -9,7 +9,7 @@ interface TestimonialProps {
 export default function Testimonial({description, author}: TestimonialProps) {
     return (
         <div
-            className={'bg-c-black w-full py-28 lg:py-40 flex flex-col justify-center px-12 md:px-20 lg:px-36 xl:px-[235px] gap-20 lg:gap-24'}>
+            className={'bg-c-black w-full py-28 lg:py-40 flex flex-col justify-center px-12 md:px-20 2xl:px-[235px] gap-20 lg:gap-24'}>
             <Image src={QuotationMark} alt={'Quotation'} className={'h-16 w-16'}/>
             <span className={'text-c-white text-4xl font-light leading-[55px] font-Inter'}>
                 {description}
