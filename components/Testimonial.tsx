5a2dd901321900f6ac7dd38a457edab05c46a6d4
Makibu <bumaciej@gmail.com>
2024-09-20 17:18:47 +0200

    Testimonial leading fix

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -11,7 +11,7 @@ export default function Testimonial({description, author}: TestimonialProps) {
         <div
             className={'bg-c-black w-full py-28 lg:py-40 flex flex-col justify-center px-12 md:px-20 lg:px-36 xl:px-[235px] gap-20 lg:gap-24'}>
             <Image src={QuotationMark} alt={'Quotation'} className={'h-16 w-16'}/>
-            <span className={'text-c-white text-4xl font-light leading-[50px] font-Inter'}>
+            <span className={'text-c-white text-4xl font-light leading-[55px] font-Inter'}>
                 {description}
             </span>
             <span className={'text-c-gray text-4xl'}>{author}</span>
